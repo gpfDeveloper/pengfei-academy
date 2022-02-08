f3b6eeb86270cc12d4207850cd0200d5075e4a0a
gpfdeveloper <pengfei.gao.2014@gmail.com>
2022-02-08 10:16:17 +0800

    course item UI update

--- components/instructor/InstructorCourseItem.js
+++ components/instructor/InstructorCourseItem.js
@@ -1,4 +1,6 @@
 import { useRouter } from 'next/router';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 import {
   Box,
@@ -16,15 +18,27 @@ const reviewStatusMap = {
 };
 
 export default function InstructorCourseItem({ item }) {
-  const { id, title, thumbnail, isPublished, reviewStatus, price } = item;
+  const {
+    id,
+    title,
+    thumbnail,
+    isPublished,
+    reviewStatus,
+    price,
+    numOfStudents,
+  } = item;
+  const theme = useTheme();
+  const isBelowSm = useMediaQuery(theme.breakpoints.down('sm'));
   const router = useRouter();
   const clickHandler = () => {
     router.push(`/instructor/course/${id}`);
   };
   return (
-    <Card sx={{ height: 140 }}>
+    <Card>
       <CardActionArea onClick={clickHandler}>
-        <Box sx={{ display: 'flex' }}>
+        <Box
+          sx={{ display: 'flex', flexDirection: isBelowSm ? 'column' : 'row' }}
+        >
           <CardMedia
             sx={{ flex: '0 0', height: 140, width: 140 }}
             component="img"
@@ -36,7 +50,7 @@ export default function InstructorCourseItem({ item }) {
             <Typography gutterBottom variant="h5" component="div">
               {title}
             </Typography>
-            <Box sx={{ display: 'flex', gap: 2 }}>
+            <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
               <Typography variant="body2" color="text.secondary">
                 {isPublished ? 'PUBLISHED' : 'DRAFT'}
               </Typography>
@@ -47,6 +61,11 @@ export default function InstructorCourseItem({ item }) {
               >
                 {reviewStatusMap[reviewStatus]}
               </Typography>
+              {numOfStudents !== 0 && (
+                <Typography variant="body2" color="text.secondary">
+                  {numOfStudents} students
+                </Typography>
+              )}
             </Box>
           </CardContent>
           {price !== undefined && (
