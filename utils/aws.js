f8c1264c387ec25d4227c94a24a320e39cf04c6c
gpfdeveloper <pengfei.gao.2014@gmail.com>
2022-03-12 09:32:41 +0800

    update aws env since it is reserved by Vercel

--- utils/aws.js
+++ utils/aws.js
@@ -1,9 +1,9 @@
 import AWS from 'aws-sdk';
 
 const awsConfig = {
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
+  region: process.env.MY_AWS_REGION,
   apiVersion: process.env.AWS_API_VERSION,
 };
 
